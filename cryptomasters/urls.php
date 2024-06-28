77f06df6bc65fa4a3b7121951456afc353100f36
mryunt02 <mrthresh58@gmail.com>
2024-06-28 23:31:53 +0300

    fresh start with php

--- cryptomasters/urls.php
+++
@@ -1,3 +0,0 @@
-<?php
-$crypto = "BTC";
-$api = "https://cex.io/api/ticker/$crypto/USD";
--- frontendmuseum/classes.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-spl_autoload_register(function ($class_name) {
-    include_once "classes/$class_name.php";
-});
